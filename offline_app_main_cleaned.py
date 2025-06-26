f2db333a3dadb6ee79d289864e82fb556cf4eaac
kgonciarz <kgonciarz@cocoasource.ch>
2025-06-26 13:40:06 +0200

    Update offline_app_main_cleaned.py

--- offline_app_main_cleaned.py
+++ offline_app_main_cleaned.py
@@ -246,19 +246,19 @@ def upload_to_sharepoint(file_buffer, filename):
         credentials = ClientCredential(client_id, client_secret)
         ctx = ClientContext(site_url).with_credentials(credentials)
 
-        # 👉 wypiszmy dokładnie co próbujemy otworzyć
         folder_url = f"/sites/TRACAFILES/{library_name}"
-        st.write(f"📁 Trying to access: {folder_url}")
-
         target_folder = ctx.web.get_folder_by_server_relative_url(folder_url)
         ctx.load(target_folder)
         ctx.execute_query()
 
         target_folder.upload_file(filename, file_buffer.getvalue()).execute_query()
         st.success(f"✅ File uploaded to SharePoint: {filename}")
+        return True
 
     except Exception as e:
         st.error(f"❌ Upload failed. Error:\n\n{e}")
+        return False
+
 
 def load_quota_view():
     result = supabase.table("quota_view").select("*").execute()
